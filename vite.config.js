17fea8d625914bf7d8940ea9034b95ce7b2aebdc
Chinedu Iroanya <iroanyahchinedu@yahoo.com>
2026-04-29 17:52:50 +0100

    Fix HTTPS proxy handling and asset URLs

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,10 @@ import laravel from 'laravel-vite-plugin'
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/js/app.tsx'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.tsx',
+            ],
             refresh: true,
         }),
     ],
