47b8effdf6acabfcab2666a1d470cbfba7c1d05b
Chinedu Iroanya <iroanyahchinedu@yahoo.com>
2026-04-29 21:19:17 +0100

    Fix cache store + Render port binding

--- resources/js/layouts/settings/layout.tsx
+++ resources/js/layouts/settings/layout.tsx
@@ -7,7 +7,7 @@ import { useCurrentUrl } from '@/hooks/use-current-url';
 import { cn, toUrl } from '@/lib/utils';
 import { edit as editAppearance } from "@/routes/appearance/index";
 import { edit } from '@/routes/profile';
-import { edit as editSecurity } from "@/routes/security/index";
+import { edit as editSecurity } from "@/routes/security";
 import type { NavItem } from '@/types';
 
 const sidebarNavItems: NavItem[] = [
