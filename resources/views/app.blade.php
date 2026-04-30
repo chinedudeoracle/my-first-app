df940b34dc89c0ed185904609f32c62a08bbb11e
Chinedu Iroanya <iroanyahchinedu@yahoo.com>
2026-05-01 00:17:21 +0100

    fix: load app CSS through Vite entry

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -37,7 +37,7 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
-        @vite(['resources/css/app.css', 'resources/js/app.tsx'])
+        @vite(['resources/js/app.tsx'])
         <x-inertia::head>
             <title>{{ config('app.name', 'Laravel') }}</title>
         </x-inertia::head>
